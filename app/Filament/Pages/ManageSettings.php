689f448b2b0e58f974ebe9f758c4fc8544049c85
Marko Pejanovic <marko.pejanovic@amplitudo.me>
2022-08-09 16:21:55 +0200

    Sve

---
+++ app/Filament/Pages/ManageSettings.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use App\Settings\FooterSettings;
+use App\Settings\GeneralSettings;
+use Filament\Forms\Components\Radio;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Pages\SettingsPage;
+
+class ManageSettings extends SettingsPage
+{
+    protected static ?string $navigationIcon = 'heroicon-o-cog';
+
+    protected static string $settings = GeneralSettings::class;
+    protected static ?string $navigationGroup = 'Settings';
+    protected function getFormSchema(): array
+    {
+        return [
+            TextInput::make('site_name')
+                ->label('Website name')
+                ->required(),
+            TextInput::make('instagram')
+                ->label('Instagram url')
+                ->required(),
+            TextInput::make('facebook')
+                ->label('Facebook url')
+                ->required(),
+            TextInput::make('youtube')
+                ->label('Youtube url')
+                ->required(),
+         
+            Toggle::make('site_active')->label('Active')->default(true)->inline(),
+        ];
+    }
+}
---
+++ app/Settings/GeneralSettings.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Settings;
+
+use Spatie\LaravelSettings\Settings;
+
+class GeneralSettings extends Settings
+{
+    public string $site_name;
+
+
+    public string $instagram;
+    public string $facebook;
+    public string $youtube;
+
+    public bool $site_active;
+
+    public static function group(): string
+    {
+        return 'general';
+    }
+}
